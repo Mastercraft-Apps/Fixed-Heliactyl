8e214edb775080130b86e54d1d72002a596587bc
achul123 <73477238+achul123@users.noreply.github.com>
2024-01-27 18:13:05 +0100

    Back

--- api/oauth2.js
+++ api/oauth2.js
@@ -11,22 +11,21 @@ if (settings.api.client.oauth2.callbackpath.slice(0, 1) !== "/")
 if (settings.pterodactyl.domain.slice(-1) == "/")
   settings.pterodactyl.domain = settings.pterodactyl.domain.slice(0, -1);
 
+const fetch = require('node-fetch');
+
 const indexjs = require("../index.js");
 const log = require('../misc/log')
 
 const fs = require("fs");
-const { renderFile } = require('ejs');
-const fetch = require('node-fetch');
+const { renderFile } = require('ejs')
 const vpnCheck = require("../misc/vpnCheck");
 
-
 module.exports.load = async function (app, db) {
-  const settings = require("../settings.json");
-  
   app.get("/login", async (req, res) => {
     if (req.query.redirect) req.session.redirect = "/" + req.query.redirect;
-    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${settings.api.client.oauth2.id}&redirect_uri=${encodeURIComponent(settings.api.client.oauth2.link + settings.api.client.oauth2.callbackpath)}&response_type=code&scope=identify%20email${settings.api.client.bot.joinguild.enabled == true ? "%20guilds.join" : ""}${settings.api.client.j4r.enabled == true ? "%20guilds" : ""}${settings.api.client.oauth2.prompt == false ? "&prompt=none" : (req.query.prompt ? (req.query.prompt == "none" ? "&prompt=none" : "") : "")}`);
-});
+    let newsettings = JSON.parse(fs.readFileSync("./settings.json"));
+    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${settings.api.client.oauth2.id}&redirect_uri=${encodeURIComponent(settings.api.client.oauth2.link + settings.api.client.oauth2.callbackpath)}&response_type=code&scope=identify%20email${newsettings.api.client.bot.joinguild.enabled == true ? "%20guilds.join" : ""}${newsettings.api.client.j4r.enabled == true ? "%20guilds" : ""}${settings.api.client.oauth2.prompt == false ? "&prompt=none" : (req.query.prompt ? (req.query.prompt == "none" ? "&prompt=none" : "") : "")}`);
+  });
 
   app.get("/logout", (req, res) => {
     let theme = indexjs.get(req);
@@ -142,7 +141,7 @@ nanobar.go(100);
           const ipuser = await db.get(`ipuser-${ip}`)
           if (ipuser && ipuser !== userinfo.id) {
             renderFile(
-              `./themes/${newsettings.theme}/alerts/alt.ejs`,
+              `./themes/${newsettings.defaulttheme}/alerts/alt.ejs`,
               {
                 settings: newsettings,
                 db,
@@ -231,7 +230,7 @@ nanobar.go(100);
           }
         }
 	      
-        // Give a discord role on login
+        //give role on login
         if (newsettings.api.client.bot.giverole.enabled == true){
           if (typeof newsettings.api.client.bot.giverole.guildid == "string" && typeof newsettings.api.client.bot.giverole.roleid == "string") {
             await fetch(
