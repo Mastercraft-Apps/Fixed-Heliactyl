090aa07fcdbced7d392e9e455ad8e83c3f99607f
achul123 <thetheatregaming@gmail.com>
2024-02-14 13:42:50 +0100

    add icon for admin fix the middle icon change some the style.css remake the sidebar update version in package lock and package fix the settings deleted import in card.js

--- api/pages.js
+++ api/pages.js
@@ -3,7 +3,7 @@ const ejs = require("ejs");
 const express = require("express");
 const settings = require("../settings.json");
 const fetch = require('node-fetch');
-const arciotext = "x";
+const arciotext = "../misc/arciotext.js";
 
 module.exports.load = async function(app, db) {
   app.all("/", async (req, res) => {
