24b5bd28c7ef6369e9c0024641ba9a0a833708c6
achul123 <73477238+achul123@users.noreply.github.com>
2024-04-01 14:53:08 +0200

    Update store.js
    Signed-off-by: achul123 <73477238+achul123@users.noreply.github.com>

--- api/store.js
+++ api/store.js
@@ -45,7 +45,7 @@ module.exports.load = async function(app, db) {
 
     adminjs.suspend(req.session.userinfo.id);
 
-    log(`Resources Purchased`, `${req.session.userinfo.username}#${req.session.userinfo.discriminator} bought ${per} ${resourceName} from the store for \`${cost}\` Credits.`);
+    log(`resources purchased`, `${req.session.userinfo.username}#${req.session.userinfo.discriminator} bought ${per} ${resourceName} from the store for \`${cost}\` Credits.`);
 
     res.redirect((theme.settings.redirect[`purchase${resourceName}`] || "/store") + "?err=none");
   };
